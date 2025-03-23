f372d24e5301f1ed027dff9d24a491860464a4d5
AmazeingGames <AmazeingGameDesign@gmail.com>
2025-03-23 16:57:45 -0700

    Fixed Candidate Hair

--- Fortune 500/Assets/Scripts/Candidate/Candidate.cs
+++ Fortune 500/Assets/Scripts/Candidate/Candidate.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Candidate : MonoBehaviour
 {
+    [Header("Sprites")]
     [SerializeField] SpriteRenderer eyes;
     [SerializeField] SpriteRenderer nose;
     [SerializeField] SpriteRenderer mouth;
@@ -20,6 +21,9 @@ public class Candidate : MonoBehaviour
         nose.sprite = candidateData.Nose;
         mouth.sprite = candidateData.Mouth;
         hairFront.sprite = candidateData.HairFront;
+        float hairFrontZ = hairFront.transform.localPosition.z;
+        hairFront.transform.localPosition = (Vector2.zero + candidateData.HairFrontOffset);
+        hairFront.transform.localPosition = new Vector3(hairFront.transform.localPosition.x, hairFront.transform.localPosition.y, hairFrontZ);
         hairBack.sprite = candidateData.HairBack;
         skin.sprite = candidateData.Skin;
         torso.sprite = candidateData.Torso;
